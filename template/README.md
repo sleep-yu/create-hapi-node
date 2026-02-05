d6760416befc3f2f180b9617aa0d32147143bf90
何红妤#A05018 <hongyu.he@casstime.com>
2026-02-05 17:54:59 +0800

    添加模版文件

---
+++ template/README.md
@@ -0,0 +1,7 @@
+# {{projectName}}
+
+Hapi + TypeScript 项目
+
+## 安装依赖
+```bash
+npm install
---
+++ template/src/main.ts
@@ -0,0 +1,18 @@
+import { Server } from "@hapi/hapi";
+
+const init = async () => {
+  const server = new Server({
+    port: 5000,
+    host: 'localhost'
+  })
+
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
+}
+
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
