ceb0c793bc0596825f627267492501c754230f14
何红妤#A05018 <hongyu.he@casstime.com>
2026-02-05 18:05:39 +0800

    完善main文件

--- template/src/main.ts
+++ template/src/main.ts
@@ -1,18 +1,43 @@
-import { Server } from "@hapi/hapi";
+import moduleAlias from 'module-alias';
 
-const init = async () => {
-  const server = new Server({
-    port: 5000,
-    host: 'localhost'
-  })
-
-  await server.start();
-  console.log('Server running on %s', server.info.uri);
-}
-
-process.on('unhandledRejection', (err) => {
-  console.log(err);
-  process.exit(1);
+// 配置路径别名
+moduleAlias.addAliases({
+  '@routes': __dirname + '/routes',
+  '@services': __dirname + '/services',
+  '@clients': __dirname + '/clients',
+  '@interfaces': __dirname + '/interfaces',
+  '@enum': __dirname + '/enum',
+  '@utils': __dirname + '/utils',
+  '@validates': __dirname + '/validates',
 });
 
-init();
+import { Server } from '@hapi/hapi';
+import { App } from './app';
+
+const PORT = process.env.PORT || 5000;
+
+(async function run() {
+  try {
+    const app = new App(PORT);
+    await app.start();
+    console.log(`服务已启动，端口: ${PORT}`);
+
+    const gracefulShutdown = async () => {
+      try {
+        await app.stop();
+      } finally {
+        process.exit(0);
+      }
+    };
+
+    process.on('SIGTERM', gracefulShutdown);
+    process.on('SIGINT', gracefulShutdown);
+
+    process.on('unhandledRejection', (err) => {
+      console.error('[unhandledRejection] 未处理Promise异常', err);
+    });
+  } catch (err) {
+    console.error('启动应用失败！', err);
+    process.exit(1);
+  }
+})();
